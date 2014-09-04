bd32ba00987f3fc82dd7045ffbdb2cf152471743
Steve North <steve@62design.co.uk>
2014-09-04 11:45:49 +0100

    Added REGEX as special chars in tab name caused error. Added 40px margin top & bottom.

--- tabbed-simple.php
+++ tabbed-simple.php
@@ -1,13 +1,12 @@
-<?php $block->open('header'); ?>
+<?php $block->open('div'); ?>
     <div class='tab-wrap'>
         <ul class="nav nav-tabs">
         <?php
         $i = 0;
         $block->document->each('@list', function ($prop, $value) use ($block, &$i) {
-            $addID = strtolower(str_replace(' ', '-', $value['name'])); ?>
+            $addID = strtolower(preg_replace("/[^A-Za-z0-9]/", "", $value['name'])); ?>
             <li<?= ($i == 0 ? ' class="active"' : '') ?>>
                 <a href="#<?= $addID ?>" data-toggle="tab"><?= $value['name'] ?></a>
-                <b class="caret"></b>
             </li>
             <?php
                 $i++;
@@ -19,7 +18,7 @@
     <?php 
     $i = 0;
     $block->document->each('@list', function ($value, $group) use ($block, &$i) {
-        $addID = strtolower(str_replace(' ', '-', $group['name'])); ?>
+        $addID = strtolower(preg_replace("/[^A-Za-z0-9]/", "", $group['name'])); ?>
         <div class="tab-pane<?= ($i == 0 ? ' active' : '') ?>" id="<?= $addID ?>">
             <?php
                 $group->tag('h1', 'name', null);
